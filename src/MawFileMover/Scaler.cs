02880d24e24ae041baaef48944e63a6caccb57ca
Mike Morano <mmorano@mikeandwan.us>
2025-05-11 09:32:09 -0400

    generate a poster image for videos

--- src/MawFileMover/Scaler.cs
+++ src/MawFileMover/Scaler.cs
@@ -75,29 +75,33 @@ public class Scaler
                 return;
             }
 
-            var dst = new FileInfo(
-                Path.Combine(src.Directory!.Parent!.FullName, scale.Code, $"{Path.GetFileNameWithoutExtension(src.Name)}.mp4")
-            );
+            var dst = Path.Combine(src.Directory!.Parent!.FullName, scale.Code, Path.GetFileNameWithoutExtension(src.Name));
+            var dstMovie = new FileInfo($"{dst}.mp4");
+            var dstPoster = new FileInfo($"{dst}.poster.avif");
+            List<(FileInfo dstFile, bool isPoster)> queue = [(dstMovie, false), (dstPoster, true)];
 
-            SafeCreateDir(dst.DirectoryName!);
+            SafeCreateDir(dstMovie.DirectoryName!);
 
-            var psi = new ProcessStartInfo
+            foreach(var (dstFile, isPoster) in queue)
             {
-                FileName = "ffmpeg",
-                Arguments = GetFfmpegArgs(src.FullName, dst.FullName, scale),
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            };
+                var psi = new ProcessStartInfo
+                {
+                    FileName = "ffmpeg",
+                    Arguments = GetFfmpegArgs(src.FullName, dstFile.FullName, scale, isPoster),
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
 
-            using var process = new Process
-            {
-                StartInfo = psi
-            };
+                using var process = new Process
+                {
+                    StartInfo = psi
+                };
 
-            process.Start();
-            await process.WaitForExitAsync(token);
+                process.Start();
+                await process.WaitForExitAsync(token);
+            }
         });
     }
 
@@ -135,7 +139,7 @@ public class Scaler
 
     // https://trac.ffmpeg.org/wiki/Encode/AV1#SVT-AV1
     // https://www.ffmpeg.org/ffmpeg-all.html#scale-1
-    static string GetFfmpegArgs(string src, string dst, Scale scale)
+    static string GetFfmpegArgs(string src, string dst, Scale scale, bool isPoster)
     {
         List<string> args = [
             "-i", $"\"{src}\""
@@ -146,7 +150,7 @@ public class Scaler
             // scale video to fit area (full height or width, with borders as necessary)
             //"-vf", $"\"scale={scale.Width}:{scale.Height}:force_original_aspect_ratio=decrease,pad={scale.Width}:{scale.Height}:(ow-iw)/2:(oh-ih)/2\"",
 
-            // scale video to fit area (cropped to fit)
+            // scale video to fit area (cropped to fit), drop sound, 24fps
             args.AddRange([
                 "-an",
                 "-vf", $"\"scale=(iw*sar)*max({scale.Width}/(iw*sar)\\,{scale.Height}/ih):ih*max({scale.Width}/(iw*sar)\\,{scale.Height}/ih), crop={scale.Width}:{scale.Height}\"",
@@ -159,6 +163,14 @@ public class Scaler
                 "-c:a", "aac",
                 "-b:a", "128k",
                 "-vf", $"\"scale={scale.Width}:{scale.Height}:force_original_aspect_ratio=decrease:force_divisible_by=2\""
+            ]);
+        }
+
+        if (isPoster)
+        {
+            args.AddRange([
+                "-ss", "00:00:02",
+                "-frames:v", "1"
             ]);
         }
 
