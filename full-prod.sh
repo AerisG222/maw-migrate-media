e7c83514cdf7e30d632c615e1542822fac1a89eb
Mike Morano <mmorano@mikeandwan.us>
2025-07-18 19:19:45 -0400

    initial cut at full migration script, which only performs media migration initially

---
+++ full-prod.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+ROOTDIR=$(pwd)
+
+# db migrate
+#echo "-- DB MIGRATE --"
+#cd src/MawDbMigrate
+
+#dotnet run \
+#    "${MAW_API_Environment__DbConnectionString}" \
+#    "${ROOTDIR}/output-db-migrate/"
+
+#cd -
+
+# migrate media
+echo "-- MIGRATE MEDIA --"
+./src/MawMediaMigrate/bin/Release/net9.0/MawMediaMigrate \
+    /data/www/website_assets \
+    /data/maw-media-assets \
+    "${ROOTDIR}/output-migrate-dry-run/" 
+
+# sql update
+#echo "-- SQL UPDATE --"
+#cd src/MawMediaSqlUpdate
+
+#dotnet run \
+#    /data/www/website_assets \
+#    /data/maw-media-assets \
+#    "${ROOTDIR}/output-migrate-dry-run/" \
+#    "${ROOTDIR}/output-sql-update/"
+
+#cd -
+
+#chmod u+x "${ROOTDIR}/output-db-migrate/import.sh"
+#chmod u+x "${ROOTDIR}/output-sql-update/import.sh"
+
+echo "** complete **"
+echo ""
+#echo "to update dev database, review and execute the following in their directories:"
+#echo "    - output-db-migrate/import.sh"
+#echo "    - output-sql-update/import.sh"
+#echo ""
